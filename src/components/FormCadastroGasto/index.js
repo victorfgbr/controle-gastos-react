e97b0372afc653a45e4c01e2cc13479d6df68f00
Victor Goulart <victorfgbr@gmail.com>
2020-11-30 18:08:09 -0300

    Inclusão dos fontes no repositório.

---
+++ src/components/FormCadastroGasto/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormCadastroGasto = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FormCadastroGasto
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import "./style.css";
+
+const Header = () => (
+    
+    <header id="main-header"> 
+        <div className="container">
+            <a href="/">
+                Controle de Gastos
+            </a>
+        </div>
+
+    </header>
+    
+);
+
+export default Header;
